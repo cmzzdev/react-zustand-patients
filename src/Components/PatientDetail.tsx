491b01a659051ddfe56b2162fde821e4274f4a7c
Carles <carmar85@gmail.com>
2024-10-18 11:57:23 +0200

    initial commit

---
+++ src/Components/PatientDetail.tsx
@@ -0,0 +1,9 @@
+import { Patient } from "../types";
+
+type PatientDetailProps = {
+  patient: Patient;
+};
+
+export default function PatientDetail({ patient }: PatientDetailProps) {
+  return <div>{patient.name}</div>;
+}
